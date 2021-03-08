3a69b1add17c1cc11abd2a1b2791eb1beda46752
unknown <yanch22@gmail.com>
2021-03-08 20:23:37 +0200

    Remove unnecessary file and some imports

--- sphera/src/components/Table/Table.js
+++ sphera/src/components/Table/Table.js
@@ -25,13 +25,6 @@ import MenuItem from '@material-ui/core/MenuItem';
 
 import trim from '../SharedObjects/WordTrimmer';
 
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-
 import './Table.css';
 import DeletePopup from "../Popup/DeletePopup.jsx";
 
